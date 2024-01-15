9ef252ef9a9708170003e1d26cc45779239cdcfe
Vladislav Perfiliev <vladis.perfiljev2011@yandex.ru>
2024-01-15 21:42:25 +0400

    First revision

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,15 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final Util connect;
+    private final Connection connection;
 
     public UserDaoJDBCImpl() {
-        connect = new Util();
+        connection = Util.getConnection();
     }
 
     public void createUsersTable() {
-        try (Connection connection = connect.getConnection();
-             Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("""
                     CREATE TABLE users (
                       `id` INT PRIMARY KEY AUTO_INCREMENT,
@@ -32,8 +31,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try (Connection connection = connect.getConnection();
-             Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("DROP TABLE users");
             System.out.println("Table \"Users\" has been deleted");
         } catch (SQLSyntaxErrorException e) {
@@ -44,10 +42,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = connect.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.executeUpdate(String.format("INSERT users (name, lastname, age) VALUES ('%s', '%s', %d)",
-                                                    name, lastName, age));
+        String sql = "INSERT users (name, lastname, age) VALUES (?, ?, ?)";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setByte(3, age);
+            ps.executeUpdate();
             System.out.printf("User %s has been added to the database\n", name);
         } catch (SQLSyntaxErrorException e) {
             System.out.println("The \"Users\" table hasn't found");
@@ -57,10 +57,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        String statementString = String.format("DELETE FROM users WHERE id = %d", id);
-        try (Connection connection = connect.getConnection();
-             Statement statement = connection.createStatement()) {
-            String result = statement.executeUpdate(statementString) == 1
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            String result = ps.executeUpdate() == 1
                     ? "User %d has been removed\n"
                     : "ID %d has not found\n";
             System.out.printf(result, id);
@@ -73,8 +73,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>(0);
-        try (Connection connection = connect.getConnection();
-             Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM users");
             resultSet.next();
             int size = resultSet.getInt(1);
@@ -91,7 +90,7 @@ public class UserDaoJDBCImpl implements UserDao {
                     user.setLastName(resultSet.getString(3));
                     user.setAge(resultSet.getByte(4));
                     userList.add(user);
-                    System.out.println(user.toString());
+                    System.out.println(user);
                 }
             }
         } catch (SQLSyntaxErrorException e) {
@@ -103,8 +102,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = connect.getConnection();
-             Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("TRUNCATE TABLE users");
             System.out.println("Table \"Users\" has been cleaned");
         } catch (SQLSyntaxErrorException e) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,26 +1,19 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.*;
-import java.util.Scanner;
 
 public class Util {
-    private final String username = "st_admin";
-    private final String password = "st_admin";
-/*
-    public Util () {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter username:");
-        username = scan.nextLine();
-        System.out.println("Enter password:");
-        password = scan.nextLine();
-        try (Connection connection = getConnection()) {
-            System.out.println("Successful login");
+    private final static String URL = "jdbc:mysql://localhost:3306/for_pp";
+    private final static String USERNAME = "st_admin";
+    private final static String PASSWORD = "st_admin";
+
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect the database!", e);
         }
-    }
-*/
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/for_pp", username, password);
+        return connection;
     }
 }
