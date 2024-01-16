b1694865ba60e08b8e683aab52615ee9cc66420c
Vladislav Perfiliev <vladis.perfiljev2011@yandex.ru>
2024-01-16 19:09:54 +0400

    2nd revision

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,10 +6,13 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         UserService user = new UserServiceImpl();
+
+        user.createUsersTable();
         user.createUsersTable();
         user.saveUser("Name1", "LastName1", (byte) 20);
         user.saveUser("Name2", "LastName2", (byte) 25);
         user.saveUser("Name3", "LastName3", (byte) 31);
+        user.removeUserById(3);
         user.saveUser("Name4", "LastName4", (byte) 38);
         user.getAllUsers();
         user.cleanUsersTable();
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
     private final Connection connection;
 
     public UserDaoJDBCImpl() {
@@ -17,86 +18,62 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("""
-                    CREATE TABLE users (
+                    CREATE TABLE IF NOT EXISTS users (
                       `id` INT PRIMARY KEY AUTO_INCREMENT,
                       `name` VARCHAR(30),
                       `lastname` VARCHAR(30),
                       `age` TINYINT(3))""");
-            System.out.println("Table \"Users\" has been created");
-        } catch (SQLSyntaxErrorException e) {
-            System.out.println("The \"Users\" table already exists");
         } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+            System.out.println("Creating the table failed");
         }
     }
 
     public void dropUsersTable() {
         try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DROP TABLE users");
-            System.out.println("Table \"Users\" has been deleted");
-        } catch (SQLSyntaxErrorException e) {
-            System.out.println("The \"Users\" table hasn't found");
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
         } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+            System.out.println("Removing the table failed");
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT users (name, lastname, age) VALUES (?, ?, ?)";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        try (PreparedStatement ps = connection.prepareStatement("INSERT users (name, lastname, age) VALUES (?, ?, ?)")) {
             ps.setString(1, name);
             ps.setString(2, lastName);
             ps.setByte(3, age);
             ps.executeUpdate();
             System.out.printf("User %s has been added to the database\n", name);
-        } catch (SQLSyntaxErrorException e) {
-            System.out.println("The \"Users\" table hasn't found");
         } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+            System.out.println("Saving the User failed");
         }
     }
 
     public void removeUserById(long id) {
-        String sql = "DELETE FROM users WHERE id = ?";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        try (PreparedStatement ps = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
             ps.setLong(1, id);
-            String result = ps.executeUpdate() == 1
-                    ? "User %d has been removed\n"
-                    : "ID %d has not found\n";
-            System.out.printf(result, id);
-        } catch (SQLSyntaxErrorException e) {
-            System.out.println("The \"Users\" table hasn't found");
+            ps.executeUpdate();
         } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+            System.out.println("Removing the User failed");
         }
     }
 
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>(0);
+
         try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM users");
-            resultSet.next();
-            int size = resultSet.getInt(1);
-            if (size == 0) {
-                System.out.println("There are no users in the table");
-            } else {
-                userList = new ArrayList<>(size);
-                resultSet = statement.executeQuery("SELECT * FROM users");
-                System.out.println("List of users:");
-                while (resultSet.next()) {
-                    User user = new User();
-                    user.setId(resultSet.getLong(1));
-                    user.setName(resultSet.getString(2));
-                    user.setLastName(resultSet.getString(3));
-                    user.setAge(resultSet.getByte(4));
-                    userList.add(user);
-                    System.out.println(user);
-                }
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+                userList.add(user);
+                System.out.println(user);
             }
-        } catch (SQLSyntaxErrorException e) {
-            System.out.println("The \"Users\" table hasn't found");
         } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+            System.out.println("Getting Users failed");
         }
         return userList;
     }
@@ -104,11 +81,8 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("TRUNCATE TABLE users");
-            System.out.println("Table \"Users\" has been cleaned");
-        } catch (SQLSyntaxErrorException e) {
-            System.out.println("The \"Users\" table hasn't found");
         } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+            System.out.println("Cleaning the table failed");
         }
     }
 }
